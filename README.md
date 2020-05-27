17228a48c1b91d05768826a7e26faae038d982ab
jsabath <justin.sabath@aol.com>
2020-05-27 05:41:31 -0600

    First start to Calendar

---
+++ README.md
@@ -0,0 +1,4 @@
+# J-Cal3
+## Modern Calendar Webapp
+### Run bundle install in the api
+
---
+++ j-cal_api/app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+    before_action :authenticate, :only [:index]
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
+    def show
+            @user = User.find(params[:id])
+            render json: @user
+        end
+
+    def create
+        @user = User.create(user_params)
+        render json: @user
+    end
+
+    def update
+        @user.update(user_params)
+        render json: @user
+    end
+
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
